95156d3f55d9aeefd209ebffff526794bcc70646
Guilherme Aquino <guiiaquino92@gmail.com>
2023-03-04 17:23:17 -0300

    feat(CSS-in-JS): Loading Component

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import {ActivityIndicator} from 'react-native'
 import { ThemeProvider } from "styled-components";
 import {
   useFonts,
@@ -6,6 +5,7 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
+import { Loading } from '@components/Loading';
 import theme from "./src/theme";
 
 import { Groups } from "@screens/Groups";
@@ -17,7 +17,7 @@ export default function App() {
   
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : <ActivityIndicator/> }
+      { fontsLoaded ? <Groups /> : <Loading/> }
     </ThemeProvider>
   );
 }
