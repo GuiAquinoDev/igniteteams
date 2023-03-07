c9d58dc599a8947060f9743cf68d636534783d75
Guilherme Aquino <guiiaquino92@gmail.com>
2023-03-07 08:17:31 -0300

    feat(CSS-in-JS): List empty

--- src/components/HighLight/index.tsx
+++ src/components/HighLight/index.tsx
@@ -5,7 +5,7 @@ type Props = {
     subtitle: string;
 }
 
-export function HightLight({title, subtitle}: Props) {
+export function Highlight({title, subtitle}: Props) {
     return (
         <Container>
             <Title>{ title }</Title>
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,26 +1,37 @@
-import { GroupCard } from "@components/GroupCard";
-import { Header } from "@components/Header";
-import { HightLight } from "@components/HighLight";
-import { useState } from "react";
-import { FlatList } from "react-native";
-import { Container} from "./styles";
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { GroupCard } from '@components/GroupCard';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/HighLight';
+
+import { Container } from './styles';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
-
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket'])
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
       <Header />
-      <HightLight title="Turmas" subtitle="jogue com a sua turma" />
-
-      <FlatList
-        data={groups}
-        keyExtractor={item => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+      <Highlight 
+        title="Turmas"
+        subtitle="jogue com sua turma"
       />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   );
 }
