5b85a60ac8c339a4177d49fe2ea19f70acf9e3cf
Guilherme Aquino <guiiaquino92@gmail.com>
2023-03-15 21:28:25 -0300

    feat: dynamic icon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,8 +1,9 @@
-
 import { Header } from "@components/Header";
 import { Highlight } from "@components/HighLight";
+import { ButtonIcon } from "@components/ButtonIcon";
 
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+import { Input } from "@components/Input";
 
 export function Players() {
   return (
@@ -13,6 +14,17 @@ export function Players() {
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
+
+      <Form>
+        <Input 
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+
+        <ButtonIcon 
+          icon="add" 
+        />
+      </Form>
     </Container>
   )
 }
