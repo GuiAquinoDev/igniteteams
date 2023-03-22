51009c34786ea6902b698c9ba33f9b5a3dfd3b2d
Guilherme Aquino <guiiaquino92@gmail.com>
2023-03-21 22:55:24 -0300

    feat(routes): navigation structure

--- App.tsx
+++ App.tsx
@@ -1,27 +1,26 @@
-import { StatusBar } from "react-native";
-import { ThemeProvider } from "styled-components";
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native'; 
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { Loading } from "@components/Loading";
-import theme from "./src/theme";
+import theme from './src/theme';
 
-import { Players } from '@screens/Players';
+import { Loading } from '@components/Loading';
+
+import { Routes } from './src/routes';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor='transparent'
-        translucent
-      />
-        { fontsLoaded ? <Players/> : <Loading/> }
+      <>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        { fontsLoaded ? <Routes /> : <Loading /> }
+      </>
     </ThemeProvider>
   );
 }
