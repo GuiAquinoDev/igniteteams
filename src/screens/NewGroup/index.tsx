2a31c6fcdf5236ef2425b3c4701cfb2b13c9485b
Guilherme Aquino <guiiaquino92@gmail.com>
2023-03-24 20:54:19 -0300

    feat(routes): use SafeAreaView

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,7 +4,7 @@ import { Container, Content, Icon } from "./styles";
 
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
-import { Highlight } from "@components/Highlight";
+import { Highlight } from "@components/HighLight";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
